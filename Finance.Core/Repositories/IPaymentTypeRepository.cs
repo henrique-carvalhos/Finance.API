30e69fa7c444dd779ac864ae88f9bf9785e25254
Henrique carvalho <henriqueart7@gmail.com>
2024-11-16 00:30:21 -0300

    implementando PaymentTypeRepository

---
+++ Finance.Core/Repositories/IPaymentTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Core.Entities;
+
+namespace Finance.Core.Repositories
+{
+    public interface IPaymentTypeRepository
+    {
+        Task<PaymentType?> GetById(int id);
+        Task<List<PaymentType>> GetAll(string search);
+        Task<int> Add(PaymentType payment);
+        Task Update(PaymentType payment);
+    }
+}
---
+++ Finance.Infrastructure/Persistence/Repositories/PaymentTypeRepository.cs
@@ -0,0 +1,39 @@
+﻿using Finance.Core.Entities;
+using Finance.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance.Infrastructure.Persistence.Repositories
+{
+    public class PaymentTypeRepository : IPaymentTypeRepository
+    {
+        private readonly IPaymentTypeRepository _repository;
+        public PaymentTypeRepository(IPaymentTypeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<int> Add(PaymentType payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PaymentType>> GetAll(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PaymentType?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(PaymentType payment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
