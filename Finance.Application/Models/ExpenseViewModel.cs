7210bebd2626b8eabb2f3f311650f029e32fb021
Henrique carvalho <henriqueart7@gmail.com>
2024-11-16 17:32:31 -0300

    adicionando card a ExpenseViewModel

--- Finance.Application/Models/ExpenseViewModel.cs
+++ Finance.Application/Models/ExpenseViewModel.cs
@@ -4,12 +4,13 @@ namespace Finance.Application.Models
 {
     public class ExpenseViewModel
     {
-        public ExpenseViewModel(int id, string description, decimal amount, DateTime dateExpense, string nameUser, string nameExpenseCategory, string nameExpenseType)
+        public ExpenseViewModel(int id, string description, decimal amount, DateTime dateExpense, string card,string nameUser, string nameExpenseCategory, string nameExpenseType)
         {
             Id = id;
             Description = description;
             Amount = amount;
             DateExpense = dateExpense;
+            Card = card;
             NameUser = nameUser;
             NameExpenseCategory = nameExpenseCategory;
             NameExpenseType = nameExpenseType;
@@ -19,11 +20,12 @@ namespace Finance.Application.Models
         public string Description { get; private set; }
         public decimal Amount { get; private set; }
         public DateTime DateExpense { get; private set; }
+        public string Card { get; set; }
         public string NameUser { get; private set; }
         public string NameExpenseCategory { get; private set; }
         public string NameExpenseType { get; private set; }
 
         public static ExpenseViewModel FromEntity(Expense expense)
-            => new(expense.Id, expense.Description, expense.Amount, expense.DateExpense, expense.User.Name, expense.ExpenseCategory.Name, expense.ExpenseType.Name);
+            => new(expense.Id, expense.Description, expense.Amount, expense.DateExpense, expense.Card.ToString(),expense.User.Name, expense.ExpenseCategory.Name, expense.ExpenseType.Name);
     }
 }
