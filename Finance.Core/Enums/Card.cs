880cfd2be58c8009e129254501cfdf03471d2861
Henrique carvalho <henriqueart7@gmail.com>
2024-11-16 17:08:01 -0300

    create card enum

---
+++ Finance.Core/Enums/Card.cs
@@ -0,0 +1,12 @@
+﻿namespace Finance.Core.Enums
+{
+    public enum Card
+    {
+        Nenhum = 0,
+        C6 = 1,
+        Itau = 2,
+        Nubank = 3,
+        Digio = 4,
+        Rico = 5
+    }
+}
