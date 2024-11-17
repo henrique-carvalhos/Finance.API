8919f5ebff598961ce24775f00830d01ca47f04c
Henrique carvalho <henriqueart7@gmail.com>
2024-11-16 22:56:12 -0300

    implementando CreateExpenseTypeValidator

---
+++ Finance.Application/Validators/CreateExpenseTypeValidator.cs
@@ -0,0 +1,17 @@
+﻿using Finance.Application.Commands.CreateExpenseType;
+using FluentValidation;
+
+namespace Finance.Application.Validators
+{
+    public class CreateExpenseTypeValidator : AbstractValidator<CreateExpenseTypeCommand>
+    {
+        public CreateExpenseTypeValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazia")
+                .MaximumLength(50)
+                .WithMessage("Tamanho máximo é de 50 caracteres");
+        }
+    }
+}
