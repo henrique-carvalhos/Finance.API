1264ec4211f576eca2df3a90c467e81f3a6d51d4
Henrique carvalho <henriqueart7@gmail.com>
2024-11-16 22:32:36 -0300

    implementando CreateUserValidator

---
+++ Finance.Application/Validators/CreateUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using Finance.Application.Commands.CreateUser;
+using FluentValidation;
+
+namespace Finance.Application.Validators
+{
+    public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .WithMessage("Nome não pode ser vazio")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+        }
+    }
+}
