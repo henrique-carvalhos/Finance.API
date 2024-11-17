56e4b55d8ef4effc3ff97e2912994ccdefbaae6b
Henrique carvalho <henriqueart7@gmail.com>
2024-11-16 22:50:44 -0300

    implementando CreateIncomeValidator

---
+++ Finance.Application/Validators/CreateIncomeValidator.cs
@@ -0,0 +1,43 @@
+﻿using Finance.Application.Commands.CreateIncome;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace Finance.Application.Validators
+{
+    internal class CreateIncomeValidator : AbstractValidator<CreateIncomeCommand>
+    {
+        public CreateIncomeValidator()
+        {
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .WithMessage("Descrição não pode ser vazia")
+                .MaximumLength(100)
+                .WithMessage("Tamanho máximo é de 100 caracteres");
+
+            RuleFor(p => p.Amount)
+                .NotEmpty()
+                .WithMessage("Valor deve ser preenchido")
+                .Must(ValidateNumber)
+                .WithMessage("Campo só aceita valores numéricos");
+
+            RuleFor(p => p.Date)
+                .NotEmpty()
+                .WithMessage("Data deve ser preenchido");
+
+            RuleFor(p => p.IncomeType)
+                .NotEmpty()
+                .WithMessage("Tipo de receita deve ser preenchido");
+
+            RuleFor(p => p.IdUser)
+                .NotEmpty()
+                .WithMessage("Id do usuário não foi preenchido, e é necessário na criação");
+        }
+
+        public bool ValidateNumber(decimal number)
+        {
+            var regex = new Regex(@"(?=.*[0-9])");
+
+            return regex.IsMatch(number.ToString());
+        }
+    }
+}
